27af71017bb536eb1042223037faa6ebe4a88d34
Yuri Cunha <cunha.ysc@gmail.com>
2023-12-11 22:11:08 -0300

    setting up project

---
+++ Musistic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Musistic
+//
+//  Created by Yuri Cunha on 11/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Musistic/MusisticApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusisticApp.swift
+//  Musistic
+//
+//  Created by Yuri Cunha on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MusisticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
