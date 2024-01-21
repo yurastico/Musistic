92a0576c2aa9628eaeb0484ceae91796709430b8
yurastico <cunha.ysc@gmail.com>
2024-01-20 22:10:45 -0300

    setting up project

---
+++ Musistic/Networking/Base/SpotifyConstants.swift
@@ -0,0 +1,14 @@
+//
+//  SpotifyConstants.swift
+//  Musistic
+//
+//  Created by Yuri Cunha on 17/01/24.
+//
+
+import Foundation
+
+enum SpotifyConstants: String {
+    case clientId = "d314e6c345724925acf57c76548ffc2c"
+    case clientSecret = "36d161ac9d8244bbba70e7caecf67536"
+    case scope = "user-read-private user-read-email user-top-read"
+}
