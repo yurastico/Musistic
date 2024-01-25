9b8d1611ed8863447f2af1bd62e860086c13e22b
yurastico <cunha.ysc@gmail.com>
2024-01-25 15:46:55 -0300

    creating artists albums model

---
+++ Musistic/Models/ArtistsAlbuns.swift
@@ -0,0 +1,18 @@
+//
+//  ArtistsAlbuns.swift
+//  Musistic
+//
+//  Created by Yuri Cunha on 25/01/24.
+//
+
+import Foundation
+
+struct ArtistsAlbuns: Codable {
+    let href: String
+    let limit: Int
+    let next: String?
+    let offset: Int
+    let previous: String?
+    let total: Int
+    let items: [Album]
+}
